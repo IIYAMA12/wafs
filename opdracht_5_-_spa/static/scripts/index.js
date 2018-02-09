b04e43425d1519530127f3d438e0c09d2abbcdd3
Kangyun Wang <wky@110.220.028.145.hva.nl>
2018-02-09 15:55:36 +0100

    Re indentation

--- opdracht_5_-_spa/static/scripts/index.js
+++ opdracht_5_-_spa/static/scripts/index.js
@@ -2,41 +2,41 @@
 	let app;
 
 	app = {
-		 init : function () {
-			  this.routes.init();
-		 },
-		 routes : {
-			  init : function () {
+		init: function () {
+			this.routes.init();
+		},
+		routes: {
+			init: function () {
 
 
-					// navigate directly when visiting the page without the navigation. Else it might get bugged, because the hash isn't triggered when it is the same url.
-					let sectionName = window.location.href.split('#')[1];
-					if (sectionName != undefined) {
-						 app.sections.toggle(sectionName);
-					}
+				// navigate directly when visiting the page without the navigation. Else it might get bugged, because the hash isn't triggered when it is the same url.
+				let sectionName = window.location.href.split('#')[1];
+				if (sectionName != undefined) {
+					app.sections.toggle(sectionName);
+				}
 
-					/*
-						 MDN web docs: "The hashchange event is fired when the fragment identifier of the URL has changed (the part of the URL that follows the # symbol, including the # symbol)."
-						 https://developer.mozilla.org/en-US/docs/Web/Events/hashchange
-					*/
-					window.addEventListener("hashchange", function (e) {
-						 let sectionName = e.newURL.split('#')[1];
-						 app.sections.toggle(sectionName);
-					});
-			  },
-		 },
-		 sections : {
-			  toggle : function (route) {
+				/*
+					 MDN web docs: "The hashchange event is fired when the fragment identifier of the URL has changed (the part of the URL that follows the # symbol, including the # symbol)."
+					 https://developer.mozilla.org/en-US/docs/Web/Events/hashchange
+				*/
+				window.addEventListener("hashchange", function (e) {
+					let sectionName = e.newURL.split('#')[1];
+					app.sections.toggle(sectionName);
+				});
+			},
+		},
+		sections: {
+			toggle: function (route) {
 				const sections = document.querySelectorAll("body > *");
 
 				for (let index = 0; index < sections.length; index++) {
 					const element = sections[index];
-					element.id ==! window.location.href.split('#')[1] ?
-					element.classList.add("hidden") :
-					element.classList.remove("hidden");
+					element.id == !window.location.href.split('#')[1] ?
+						element.classList.add("hidden") :
+						element.classList.remove("hidden");
 				}
-			  },
-		 }
+			},
+		}
 	}
 
 	app.init();
